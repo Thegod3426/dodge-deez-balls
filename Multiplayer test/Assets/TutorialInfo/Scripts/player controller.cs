15a07096652715edac2e63266b0dc3531bca1587
Mightymace-coin <mightyhappyfeet@gmail.com>
2025-07-03 13:36:26 -0700

    added movment

---
+++ Multiplayer test/Assets/TutorialInfo/Scripts/player controller.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed;
+    private float horizontalInput; 
+    private float verticalInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         horizontalInput = Input.GetAxis("Horizontal");
+         verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up * speed * horizontalInput * Time.deltaTime);
+    }
+}
