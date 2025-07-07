26bdcbac750e3626c6b9730f00a9ce4c2fc1e19d
Thegod3426 <127333785+Thegod3426@users.noreply.github.com>
2025-07-07 08:23:36 -0700

    'uyt';

--- Multiplayer test/Assets/Scripts/player controller.cs
+++ Multiplayer test/Assets/Scripts/player controller.cs
@@ -7,6 +7,9 @@ public class playercontroller : MonoBehaviour
     public float turnSpeed;
     private float horizontalInput; 
     private float verticalInput;
+    public float jumpForce = 150;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,5 +25,10 @@ public class playercontroller : MonoBehaviour
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Rotate(Vector3.up * speed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
     }
 }
